42157e8f446ec4ca710ca3f628ba9bdca1730683
DESKTOP-04H9TEG\Mariola <pomimoto@gmail.com>
2018-06-03 23:18:10 +0200

    First commit

---
+++ WorkshopsEnterToSpecFlow.Tests/TestsCases/SeleniumLesson1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WorkshopsEnterToSpecFlow.Tests.TestsCases
+{
+	class SeleniumLesson1
+	{
+		private ChromeDriver driver;
+		private string testUrl = "https://suvroc.github.io/selenium-mail-course/01/example.html";
+
+		[SetUp]
+		public void Setup()
+		{
+			driver = new ChromeDriver();
+
+		}
+
+		[Test]
+		public void FirstTest()
+		{
+			var driver = new ChromeDriver();
+			driver.Navigate().GoToUrl(testUrl);
+
+			var element = driver.FindElement(By.Name("elementWithName"));
+
+			Assert.IsNotNull(element);
+			Assert.AreEqual(element.GetAttribute("value"), "Name test data");
+
+			driver.Quit();
+
+		}
+
+
+	}
+}
